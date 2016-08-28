ae0ced63a0c692a42c7d4678291b45e4282f884d
David Naber <kontakt@dnaber.de>
2016-08-28 16:40:35 +0200

    Make registry methods agg.-type aware

--- src/Aggregation/GenericRegistry.php
+++ src/Aggregation/GenericRegistry.php
@@ -4,9 +4,15 @@ namespace ElasticFacets\Aggregation;
 
 use ElasticFacets\ElasticPress\AggregationExpressionCollection;
 use ElasticFacets\Result\AggregationParserCollection;
+use ElasticFacets\Result\ResultStore;
 use ElasticFacets\Type\AggregatedNumericRangesCollection;
 use ElasticFacets\Type\AggregatedTermsCollection;
 
+/**
+ * Class GenericRegistry
+ *
+ * @package ElasticFacets\Aggregation
+ */
 final class GenericRegistry implements Registry {
 
 	/**
@@ -20,15 +26,52 @@ final class GenericRegistry implements Registry {
 	private $expression_collection;
 
 	/**
+	 * @var ResultStore
+	 */
+	private $results;
+
+	/**
+	 * @param AggregationParserCollection     $parser_collection
+	 * @param AggregationExpressionCollection $expression_collection
+	 * @param ResultStore                     $results
+	 */
+	public function __construct(
+		AggregationParserCollection $parser_collection,
+		AggregationExpressionCollection $expression_collection,
+		ResultStore $results
+	) {
+
+		$this->parser_collection     = $parser_collection;
+		$this->expression_collection = $expression_collection;
+		$this->results               = $results;
+	}
+
+	/**
 	 * @param string $id
-	 * @param object $aggregation (Any aggregation type that implements AggregationExpression AND one of the AggregationParser)
+	 * @param Terms  $aggregation
 	 *
 	 * @throws \InvalidArgumentException
 	 *
 	 * @return bool
 	 */
-	public function add_aggregation( $id, $aggregation ) {
-		// TODO: Implement add_aggregation() method.
+	public function add_terms_aggregation( $id, Terms $aggregation ) {
+
+		return $this->parser_collection->push_parser( $aggregation, $id )
+			&& $this->expression_collection->push_expression( $aggregation );
+	}
+
+	/**
+	 * ID needs to be unique across all aggregation types
+	 *
+	 * @param string        $id
+	 * @param NumericRanges $aggregation
+	 *
+	 * @return mixed
+	 */
+	public function add_numeric_ranges_aggregation( $id, NumericRanges $aggregation ) {
+
+		return $this->parser_collection->push_parser( $aggregation, $id )
+			&& $this->expression_collection->push_expression( $aggregation );
 	}
 
 	/**
@@ -48,10 +91,23 @@ final class GenericRegistry implements Registry {
 	 *
 	 * @throws \InvalidArgumentException (If no aggregation is registered for the given ID)
 	 *
-	 * @return AggregatedNumericRangesCollection|AggregatedTermsCollection
+	 * @return AggregatedTermsCollection
 	 */
-	public function get_aggregation_result( $id ) {
-		// TODO: Implement get_aggregation_result() method.
+	public function get_terms_result( $id ) {
+
+		return $this->results->terms_result( $id );
+	}
+
+	/**
+	 * @param string $id
+	 *
+	 * @throws \InvalidArgumentException (If no aggregation is registered for the given ID)
+	 *
+	 * @return AggregatedNumericRangesCollection
+	 */
+	public function get_numeric_ranges_result( $id ) {
+
+		return $this->results->numeric_ranges_result( $id );
 	}
 
 }
--- src/Aggregation/Registry.php
+++ src/Aggregation/Registry.php
@@ -5,34 +5,50 @@ namespace ElasticFacets\Aggregation;
 use ElasticFacets\Type\AggregatedNumericRangesCollection;
 use ElasticFacets\Type\AggregatedTermsCollection;
 
+/**
+ * Interface Registry
+ *
+ * @package ElasticFacets\Aggregation
+ */
 interface Registry {
 
 	/**
-	 * @param string $id
-	 * @param object $aggregation (Any aggregation type that implements AggregationExpression AND one of the AggregationParser)
+	 * ID needs to be unique across all aggregation types
 	 *
-	 * @throws \InvalidArgumentException                           
-	 *                            
-	 * @return bool
-	 */
-	public function add_aggregation( $id, $aggregation );
-
-	/**
 	 * @param string $id
-	 * @param array  $config
-	 * 
+	 * @param Terms  $aggregation
+	 *
 	 * @throws \InvalidArgumentException
 	 *
 	 * @return bool
 	 */
-	public function build_aggregation( $id, array $config );
-	
+	public function add_terms_aggregation( $id, Terms $aggregation );
+
+	/**
+	 * ID needs to be unique across all aggregation types
+	 *
+	 * @param string        $id
+	 * @param NumericRanges $aggregation
+	 *
+	 * @return mixed
+	 */
+	public function add_numeric_ranges_aggregation( $id, NumericRanges $aggregation );
+
 	/**
 	 * @param string $id
 	 *
 	 * @throws \InvalidArgumentException (If no aggregation is registered for the given ID)
-	 * 
-	 * @return AggregatedNumericRangesCollection|AggregatedTermsCollection
+	 *
+	 * @return AggregatedTermsCollection
 	 */
-	public function get_aggregation_result( $id );
+	public function get_terms_result( $id );
+
+	/**
+	 * @param string $id
+	 *
+	 * @throws \InvalidArgumentException (If no aggregation is registered for the given ID)
+	 *
+	 * @return AggregatedNumericRangesCollection
+	 */
+	public function get_numeric_ranges_result( $id );
 }
