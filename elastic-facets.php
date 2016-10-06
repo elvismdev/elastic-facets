1babee3a424c013e444a19adda8895d98aeaeb23
David Naber <kontakt@dnaber.de>
2016-10-06 18:21:04 +0200

    Start only once on main query

--- elastic-facets.php
+++ elastic-facets.php
@@ -15,6 +15,7 @@ namespace ElasticFacets;
 
 use ElasticFacets\Plugin\ElasticFacetsLoader;
 use GuzzleHttp\Psr7\ServerRequest;
+use WP_Query;
 
 add_action( 'muplugins_loaded', __NAMESPACE__ . '\init', 1 );
 
@@ -33,7 +34,14 @@ function init() {
 	$request = ServerRequest::fromGlobals();
 	add_action(
 		'parse_query',
-		[ ElasticFacetsLoader::build_with_optional_dependencies( $request ), 'register_callbacks' ],
+		function( WP_Query $query ) use ( $request ) {
+			static $loaded;
+			if ( ! $query->is_main_query() || $loaded ) {
+				return;
+			}
+			ElasticFacetsLoader::build_with_optional_dependencies( $request )->register_callbacks();
+			$loaded = TRUE;
+		},
 		20
 	);
 }
